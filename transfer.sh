a19b33851dcee1fed2bd52654d172a52db542edc
Adrián López García de Lomana <14022465+adelomana@users.noreply.github.com>
2024-05-27 18:19:54 +0000

    minor update on transfer script

--- transfer.sh
+++ transfer.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-# this is a command to transfer files from necio5 to elja. Should be run in necio5
+# this is a command to transfer files from necio5 to elja. Should be run in necio5. OK from IT on 2024.05.27
 
 rsync -azPvh --bwlimit=40000 /Users/adrian/research/keilir/data/raw_fastq -e "ssh -i ~/.ssh/elja" adrian@elja.hi.is:/hpcdata/Mimir/adrian/research/keilir/data
